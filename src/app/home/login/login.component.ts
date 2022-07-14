70e9846ea67222e51771f32eba4ec3544a7347c9
uzielmenezes <uzielcivil@gmail.com>
2022-07-14 20:41:27 -0300

    importing authentication service  to login comp

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +11,18 @@ export class LoginComponent implements OnInit {
   usuario = '';
   senha = '';
 
-  constructor() { }
+  constructor(private authService: AutenticacaoService) { }
 
   ngOnInit(): void { }
 
   login() {
-    console.log(this.usuario);
-    console.log(this.senha);
+    this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
+      console.log('Autenticado com sucesso!');
+    }, (err) => {
+      alert('Usuário ou senha inválidos!');
+      console.log(err);
+    }
+    )
   }
 
 }
