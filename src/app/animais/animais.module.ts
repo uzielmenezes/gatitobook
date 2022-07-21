57c9f8ded8bb0d8ce0a6be8ff7bbfaddfb3ec7e6
uzielmenezes <uzielcivil@gmail.com>
2022-07-21 19:24:48 -0300

    picture grades component

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -1,19 +1,14 @@
+import { CartaoModule } from './../componentes/cartao/cartao.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
-
+import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 
 @NgModule({
-  declarations: [
-    ListaAnimaisComponent,
-    AnimalComponent
-  ],
-  imports: [
-    CommonModule,
-    AnimaisRoutingModule
-  ]
+  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent],
+  imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
 })
-export class AnimaisModule { }
+export class AnimaisModule {}
