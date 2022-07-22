f1ceb55d294b0f9e43655300fcd2badaf0dba0ad
uzielmenezes <uzielcivil@gmail.com>
2022-07-22 19:13:00 -0300

    comentarios service

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -1,3 +1,5 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { CartaoModule } from './../componentes/cartao/cartao.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -7,9 +9,22 @@ import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
+import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
 
 @NgModule({
-  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent],
-  imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
+  declarations: [
+    ListaAnimaisComponent,
+    AnimalComponent,
+    GradeFotosAnimaisComponent,
+    DetalheAnimalComponent,
+    ComentariosComponent,
+  ],
+  imports: [
+    CommonModule,
+    AnimaisRoutingModule,
+    CartaoModule,
+    MensagemModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AnimaisModule {}
---
+++ src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -0,0 +1,16 @@
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Comentarios } from './comentarios';
+
+@Component({
+  selector: 'app-comentarios',
+  templateUrl: './comentarios.component.html',
+  styleUrls: ['./comentarios.component.css'],
+})
+export class ComentariosComponent implements OnInit {
+  comentarios$!: Observable<Comentarios>;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
