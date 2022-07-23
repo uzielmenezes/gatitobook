c4b63469163d69cab86cc957785e266d142b5db8
uzielmenezes <uzielcivil@gmail.com>
2022-07-23 20:56:02 -0300

    novo-animal template

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,15 +1,22 @@
+import { FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-novo-animal',
   templateUrl: './novo-animal.component.html',
-  styleUrls: ['./novo-animal.component.css']
+  styleUrls: ['./novo-animal.component.css'],
 })
 export class NovoAnimalComponent implements OnInit {
+  formularioAnimal!: FormGroup;
+  file!: File;
+  preview!: string;
+  percentualConcluido = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  upload() {}
+
+  gravaArquivo(arquivo: any) {}
 }
